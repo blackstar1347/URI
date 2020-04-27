7ad3f3a7cfa3ee0c3865f0938fc957cec06f8ec3
blackstar1347 <helder.ramos13@hotmail.com>
2020-04-27 16:43:58 -0300

    1004

---
+++ src/respostas/Resolucao_1004.java
@@ -0,0 +1,23 @@
+package respostas;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Resolucao_1004 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int A,B;
+		int PROD;
+		
+		A = entrada.nextInt();
+		B = entrada.nextInt();
+		PROD = A*B;
+		
+		System.out.print("PROD = " +PROD);
+		
+
+	}
+
+}
