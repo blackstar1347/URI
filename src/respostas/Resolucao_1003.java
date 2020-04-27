bb973f9f7778fc379328cb76dd8e50ade960e57c
blackstar1347 <helder.ramos13@hotmail.com>
2020-04-27 16:31:27 -0300

    1003

---
+++ src/respostas/Resolucao_1003.java
@@ -0,0 +1,23 @@
+package respostas;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Resolucao_1003 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int A,B;
+		int SOMA;
+		
+		 A = entrada.nextInt();
+		 B = entrada.nextInt();
+		 SOMA = (A+B);
+		 
+		 System.out.print("SOMA = " + SOMA);
+		
+
+	}
+
+}
