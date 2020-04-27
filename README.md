f0a2c402ef12f1ca3142a7e692f8f56873888096
blackstar1347 <helder.ramos13@hotmail.com>
2020-04-27 16:04:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# URI
+ Resoluções dos exercícios do site https://www.urionlinejudge.com.br/judge/
---
+++ src/respostas/Resolucao_1001.java
@@ -0,0 +1,10 @@
+package respostas;
+
+public class Resolucao_1001 {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
