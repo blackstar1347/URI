12d0464ffcad95af74c9ef460e8bef2bc5f9b493
blackstar1347 <helder.ramos13@hotmail.com>
2020-04-27 16:09:54 -0300

    1001

--- src/respostas/Resolucao_1001.java
+++ src/respostas/Resolucao_1001.java
@@ -1,10 +1,22 @@
 package respostas;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class Resolucao_1001 {
-
-	public static void main(String[] args) {
 	
-
-	}
+    public static void main(String[] args) throws IOException {
+    	Scanner scan = new Scanner(System.in);
+	
+	int A;
+	int B;
+	int X;
+	
+	A = scan.nextInt();
+	B = scan.nextInt();
+	X = A+B;
+	
+	System.out.println("X = "+X);
+    
+    }
 
 }
